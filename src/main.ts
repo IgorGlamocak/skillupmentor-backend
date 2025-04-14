440ae7e4b2abf4d79164a7f99c98446be1df9795
Igor Glamočak <igor.glamocak@scv.si>
2025-04-14 11:36:29 +0200

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './modules/app.module'
+import { ValidationPipe } from '@nestjs/common'
+import cookieParser from 'cookie-parser'
+import express from 'express'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  })
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  })
+  app.useGlobalPipes(new ValidationPipe())
+  app.use(cookieParser())
+  app.use('/files', express.static('files'))
 
   const PORT = process.env.PORT || 8080
   await app.listen(PORT)
+
+  console.log(`App is listnening on: ${await app.getUrl()}`)
 }
 bootstrap()
